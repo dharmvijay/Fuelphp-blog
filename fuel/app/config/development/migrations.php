22a9fac0293ebfbffc397e2868867b4c374f22dc
Dharmvijay Patel <dbp264@gmail.com>
2016-01-19 16:59:56 +0530

    Removed migration default updates.

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -6,9 +6,6 @@ return array (
     array (
       'default' => 
       array (
-        0 => '001_create_blogs',
-        1 => '002_create_comments',
-        2 => '003_create_users',
       ),
     ),
     'module' => 
